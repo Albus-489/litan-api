2ebfb50c724ac985b7966582197795da4f031f41
Albus-489 <bavrzar@gmail.com>
2023-05-23 14:55:37 +0300

    Module imports

--- src/litans/litans.module.ts
+++ src/litans/litans.module.ts
@@ -1,14 +1,24 @@
 import { Module } from '@nestjs/common';
-import { LitansService } from './litans.service';
-import { LitansController } from './litans.controller';
+// import { LitansService } from './controllers/litan/litans.service';
+// import { LitansController } from './controllers/litan/litans.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Litan, LitanSchema } from 'src/scheamas/litans.schema';
+import { Volume, VolumeSchema } from 'src/scheamas/volume.schema';
+import { Summary, SummarySchema } from 'src/scheamas/summary.schema';
+import { VolumesController } from './controllers/volume/volume.controller';
+import { VolumesService } from './controllers/volume/volume.service';
+import { SummariesController } from './controllers/summary/summary.controller';
+import { SummariesService } from './controllers/summary/summary.service';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Litan.name, schema: LitanSchema }]),
+    MongooseModule.forFeature([
+      // { name: Litan.name, schema: LitanSchema },
+      { name: Volume.name, schema: VolumeSchema },
+      { name: Summary.name, schema: SummarySchema },
+    ]),
   ],
-  controllers: [LitansController],
-  providers: [LitansService],
+  controllers: [VolumesController, SummariesController],
+  providers: [VolumesService, SummariesService],
 })
 export class LitansModule {}
